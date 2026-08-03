5a783698924705b7d6fce317151928d0bd30a2b1
Shawn <shawndullen@hotmail.com>
2026-08-03 17:10:43 -0400

    Declare Node console for repository scripts

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,7 @@ export default tseslint.config(
     files: ['scripts/**/*.mjs'],
     languageOptions: {
       globals: {
+        console: 'readonly',
         process: 'readonly',
       },
     },
